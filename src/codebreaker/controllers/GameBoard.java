290df8d1cdc356decbc83b7a4317051da4ef3c05
Hayley So <hayleyso99@gmail.com>
2024-12-29 22:14:52 -0500

    gameboard

---
+++ src/codebreaker/controllers/GameBoard.java
@@ -0,0 +1,13 @@
+package codebreaker.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class GameBoard {
+    @FXML
+    private ImageView background;
+
+    
+
+    
+}
---
+++ src/codebreaker/controllers/GameOverview.java
@@ -0,0 +1,24 @@
+package codebreaker.controllers;
+
+import java.io.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class GameOverview {
+
+    @FXML
+    void onNextBtnClick(ActionEvent event) throws IOException {
+        Parent gameOverviewparent = FXMLLoader.load(getClass().getResource("/codebreaker/gui/fxml/HTP1.fxml"));
+        Scene gameOverviewScene = new Scene(gameOverviewparent);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(gameOverviewScene);
+        window.show();
+    }
+
+}
